49d9129a2a24ef611da00e75724af515f88327ce
DataSign-ThinkLab <thinklab@datasign.mx>
2026-06-17 11:20:21 -0600

    Menu listo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,16 @@ import type { RouteObject } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import Aloo from '../features/Aloo';
 import Menora from '../features/Menora';
+import AlooMenu from '../features/AlooMenu';
 
 const router: RouteObject[] = [
   {
     path: '/',
     element: <Aloo />,
+  },
+  {
+    path: '/menu-aloo',
+    element: <AlooMenu />,
   },
   {
     path: '/menora',
